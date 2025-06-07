fee91bcce6c5a8ebce2b673dd8b7e6c1b562a240
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-07 17:09:39 -0300

    tpe.sh

--- tpe.sh
+++ tpe.sh
@@ -1,19 +1,29 @@
-   let prefix=$1
-    # Tira error si no se pasan los parámetros necesarios.
-    
-    # Recibe un parámetro llamado “prefix” y que devuelva el id de la primer temporada (o elemento season) cuyo nombre (atributo name) comience con lo 
-    # definido en el parámetro prefix. En caso de no haber ninguna temporada que cumpla dicha característica, debe devolver un string vacío.
+let prefix=$1
+let request_command="Invoke-WebRequest -Uri"
+let flags="-Headers @{ 'accept' = 'application/json'; 'x-api-key' = '${SPORTRADAR_API}' } -OutFile"
 
-java -cp C:\Users\ivonn\Documents\SaxonHE9-5-1-2J\saxon9he.jar  net.sf.saxon.Query -s:seasons_list.xml -q:extract_season_id.xq prefix="${prefix}"
+# bash flags: 
+# Tira error si no se pasan los parámetros necesarios.
 
-    # Dado un id de temporada (o season_id), este método devuelve características de la misma.
-    # NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
- 
-Invoke-WebRequest -Uri "https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A95495/info.xml" -Headers @{ "accept" = "application/json"; "x-api-key" = "${SPORTRADAR_API}" } -OutFile "season_info.xml"
+if [[ "$OSTYPE" == linux* || "$OSTYPE" == darwin* ]]; then
+  request_command="curl -X GET"
+  flags="--header 'accept: application/json' --header 'x-api-key: ${SPORTRADAR_API}' -o"
+fi
+
+# Recibe un parámetro llamado “prefix” y que devuelva el id de la primer temporada (o elemento season) cuyo nombre (atributo name) comience con lo 
+# definido en el parámetro prefix. En caso de no haber ninguna temporada que cumpla dicha característica, debe devolver un string vacío.
+# C:\Users\ivonn\Documents\SaxonHE9-5-1-2J\ esto no tiene que estar para el saxon9he.jar
+
+java -cp saxon9he.jar  net.sf.saxon.Query -s:seasons_list.xml -q:extract_season_id.xq prefix="${prefix}"
+
+# Dado un id de temporada (o season_id), este método devuelve características de la misma.
+# NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
+
+$request_command "https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A95495/info.xml" $flags "season_info.xml"
 
 
-    # Dado un id de temporada (o season_id), este método devuelve las estadísticas (tales como: puntos, goles a favor, goles en 
-    # contra, diferencia de goles, partidos ganados, etc) de los equipos que compitieron en ella.
-    # NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
- 
-Invoke-WebRequest -Uri "https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A95495/standings.xml" -Headers @{ "accept" = "application/json"; "x-api-key" = "${SPORTRADAR_API}" } -OutFile "season_standings.xml"
+# Dado un id de temporada (o season_id), este método devuelve las estadísticas (tales como: puntos, goles a favor, goles en 
+# contra, diferencia de goles, partidos ganados, etc) de los equipos que compitieron en ella.
+# NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
+
+$request_command "https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A95495/standings.xml" $flags "season_standings.xml"
