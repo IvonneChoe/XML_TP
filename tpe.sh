a469b3264abb93c199996c4ada82c91abec42366
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-07 22:21:31 -0300

    Correcciones en el shell script

--- tpe.sh
+++ tpe.sh
@@ -1,20 +1,25 @@
-let prefix=$1
-let request_command="Invoke-WebRequest -Uri"
-let flags="-Headers @{ 'accept' = 'application/json'; 'x-api-key' = '${SPORTRADAR_API}' } -OutFile"
+prefix=$1
 
-# bash flags: 
-# Tira error si no se pasan los parámetros necesarios.
-
-if [ "$OSTYPE" == linux* || "$OSTYPE" == darwin* ] ; then
-  request_command="curl -X GET"
-  flags="--header 'accept: application/json' --header 'x-api-key: ${SPORTRADAR_API}' -o"
+if [[ -z "$1" ]]; then
+  echo "Prefix required, use: $0 <prefix>"
+  exit 1
 fi
+
+if [[ -z "${SPORTRADAR_API}" ]]; then
+  echo "SPORTRADAR_API environment variable not setted"
+fi
+
+# request_command="Invoke-WebRequest -Uri"
+# flags="-Headers @{ 'accept' = 'application/json'; 'x-api-key' = '${SPORTRADAR_API}' } -OutFile"
+
+request_command="curl -X GET"
+flags="--header \"accept: application/json\" --header \"x-api-key: ${SPORTRADAR_API}\" -o"
 
 # Recibe un parámetro llamado “prefix” y que devuelva el id de la primer temporada (o elemento season) cuyo nombre (atributo name) comience con lo 
 # definido en el parámetro prefix. En caso de no haber ninguna temporada que cumpla dicha característica, debe devolver un string vacío.
 # C:\Users\ivonn\Documents\SaxonHE9-5-1-2J\ esto no tiene que estar para el saxon9he.jar
 
-java -cp saxon9he.jar  net.sf.saxon.Query -s:seasons_list.xml -q:extract_season_id.xq prefix="${prefix}"
+java net.sf.saxon.Query -s:seasons_list.xml -q:extract_season_id.xq prefix="${prefix}"
 
 # Dado un id de temporada (o season_id), este método devuelve características de la misma.
 # NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
