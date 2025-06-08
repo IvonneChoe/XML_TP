929f595bc82b07cf0748bce3b60144205ce0753d
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-06-08 13:44:57 -0300

    Correcciones de shellscript

--- tpe.sh
+++ tpe.sh
@@ -6,7 +6,7 @@ if [[ -z "$1" ]]; then
 fi
 
 if [[ -z "${SPORTRADAR_API}" ]]; then
-  echo "SPORTRADAR_API environment variable not setted"
+  echo 'SPORTRADAR_API environment variable not setted'
 else
   echo "${SPORTRADAR_API} is your key"
 fi
@@ -14,35 +14,21 @@ fi
 function extraction() {
   out=$1
   query=$2
-  xmllint --foramt (java net.sf.saxon.Query -s:$out -q:$query prefix="${prefix}") out
+  java net.sf.saxon.Query -s:$out -q:$query prefix="${prefix}"
 }
 
 function download() {
   url=$1
-  output_file_name=$2
-  $request_command $url $output_file_name
+  output_file=$2
+  curl -X GET "${url}" --header 'accept: application/xml' --header "x-api-key: ${SPORTRADAR_API}" > $output_file
 }
 
-# Windows comands
-# request_command="Invoke-WebRequest -Uri"
-# flags="-Headers @{ 'accept' = 'application/json'; 'x-api-key' = '${SPORTRADAR_API}' } -OutFile"
+season_id=$(extraction seasons_list.xml extract_season_id.xq $prefix | egrep -o 'season:\d+$' | egrep -o '\d+$')
 
-request_command="curl -X GET"
-flags="--header \"accept: application/json\" --header \"x-api-key: ${SPORTRADAR_API}\" -o"
+url="https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A${season_id}"
 
-# Dado un id de temporada (o season_id), este método devuelve características de la misma.
-# NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
-download "https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A${season_id}/info.xml" "season_info.xml"
+download "${url}/info.xml" season_info.xml
 
-# Dado un id de temporada (o season_id), este método devuelve las estadísticas (tales como: puntos, goles a favor, goles en 
-# contra, diferencia de goles, partidos ganados, etc) de los equipos que compitieron en ella.
-# NOTE: sr%3Aseason%3AXXXXX --> modificar XXXXX por el id de temporada que se quiera consultar.
-download "https://api.sportradar.com/handball/trial/v2/en/seasons/sr%3Aseason%3A${season_id}/standings.xml" "season_standings.xml"
+download "${url}/standings.xml" season_standings.xml
 
-# Recibe un parámetro llamado “prefix” y que devuelva el id de la primer temporada (o elemento season) cuyo nombre (atributo name) comience con lo 
-# definido en el parámetro prefix. En caso de no haber ninguna temporada que cumpla dicha característica, debe devolver un string vacío.
-# C:\Users\ivonn\Documents\SaxonHE9-5-1-2J\ esto no tiene que estar para el saxon9he.jar
-java net.sf.saxon.Query -s:seasons_list.xml -q:extract_season_id.xq prefix="${prefix}"
-
-java net.sf.saxon.Query -q:extract_handball_data.xq prefix="${prefix}" > handball_data.xml 
-
+extraction handball_data.xml extract_handball_data.xq $prefix > handball_data.xml
